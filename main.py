e4991a3df34b703628677bfc1bab270a3da1edbe
Anastasiia Yerashova <yerashova.a@gmail.com>
2026-01-08 16:19:18 +0100

    ran 30 tests

--- main.py
+++ main.py
@@ -40,26 +40,30 @@ class Record:
         if not isinstance(self.name, Name):
             self.name = Name(self.name)
 
+
     def add_phone (self, phone: str):
         new_phone = Phone(phone)
 
         if new_phone.value not in [p.value for p in self.phones]:
+
             self.phones.append(new_phone)
+
             return f'Phone number {new_phone.value} added to contact'
         
-        else:
-            return 'Phone number already exists in contact'
+        return 'Phone number already exists in contact'
+    
 
     def remove_phone (self, phone: str):
         phone_to_remove = phone.strip()
 
-        for p in self.phones:
+        if any(p.value == phone_to_remove for p in self.phones):
 
-            if p.value == phone_to_remove:
-                self.phones.remove(p)
-                return f'Phone number {phone_to_remove} removed from contact'
+            self.phones = list(filter(lambda p: p.value == phone_to_remove, self.phones))
+
+            return f'Phone number {phone_to_remove} removed from contact'
             
         raise ValueError('Phone number not found in contact')
+    
 
     def edit_phone (self, old_phone: str, new_phone: str):
         old_phone = old_phone.strip()
@@ -68,20 +72,18 @@ class Record:
         for i, p in enumerate(self.phones):
             
             if p.value == old_phone:
+
                 self.phones[i] = Phone(new_phone)
+
                 return f'Phone number {old_phone} changed to {new_phone}'
             
         raise ValueError('Old phone number not found in contact')
     
+    
     def find_phone (self, phone: str) -> Phone | None:
         phone_to_find = phone.strip()
 
-        for p in self.phones:
-
-            if p.value == phone_to_find:
-                return p
-            
-        return None
+        return next((p for p in self.phones if p.value == phone_to_find), None)
 
     def __str__(self):
         return f'Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}'
@@ -93,16 +95,17 @@ class AddressBook(UserDict):
 
         return f'Record for {record.name.value} added to address book'
     
+    
     def delete (self, name: str):
         name_to_delete = name.strip()
 
         if name_to_delete not in self.data:
             return f'Record for {name_to_delete} not found in address book'
             
-        else:
-            del self.data[name_to_delete]
+        del self.data[name_to_delete]
 
         return f'Record for {name_to_delete} deleted from address book'
+    
         
     def find (self, name: str):
         name_to_find = name.strip()
@@ -118,6 +121,7 @@ if __name__ == "__main__":
     john_record = Record("John")
     john_record.add_phone("1234567890")
     john_record.add_phone("5555555555")
+    print(john_record.__dict__)
 
    
     book.add_record(john_record)
